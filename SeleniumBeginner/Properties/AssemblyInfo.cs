b0861315de4e4a9d1406169f5ed95a62779e8b1d
Ovidiu Chiruta <ovidiu.chiruta@gmail.com>
2020-03-14 17:22:29 +0200

    Add project files.

---
+++ SeleniumBeginner/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumBeginner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumBeginner")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("492154c3-083a-44d3-b543-2a1c96754fbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumBeginner/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumBeginner
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var browser = new ChromeDriver();
+
+            browser.Navigate().GoToUrl("http://www.google.com/");
+
+            browser.FindElementByCssSelector("input.gLFyf").SendKeys("Abi Talent Velea");
+
+            browser.FindElementByName("btnK").Click();
+
+            var text = browser.FindElementByCssSelector("div.rc").Text;
+
+            var existaAbi = text.Contains("Abi Talent și conflictul cu Alex Velea - ce i-a transmis");
+
+            Assert.IsTrue(existaAbi);
+
+            //browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
+
+
+            Thread.Sleep(6000);  //set the time before quit
+
+            browser.Quit();
+                          
+        }
+    }
+}
