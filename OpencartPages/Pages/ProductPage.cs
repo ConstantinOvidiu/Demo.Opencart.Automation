df5ba2c1489d4ff90870eb0148e4a5b2e1c7ce22
Ovidiu Chiruta <ovidiu.chiruta@gmail.com>
2020-04-11 21:31:28 +0300

    Adding 4 Product Review Validation Test Methods

--- OpencartPages/Pages/ProductPage.cs
+++ OpencartPages/Pages/ProductPage.cs
@@ -15,15 +15,18 @@ namespace OpencartPages.Pages
             PageFactory.InitElements(browser, this);
         }
 
+        //Search Input/Button elements
         [FindsBy(How = How.Name, Using = "search")]
         private IWebElement searchInput { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = "button.btn.btn-default.btn-lg")]
-        private IWebElement btnSearch { get; set; }  
+        private IWebElement btnSearch { get; set; }
 
+        //Product Thumb elements
         [FindsBy(How = How.ClassName, Using = "product-thumb")]
         public IWebElement productResult { get; set; }
 
+        //Currency elements
         [FindsBy(How = How.Id, Using = "form-currency")]
         public IWebElement linkCurrency { get; set; } 
 
@@ -34,7 +37,36 @@ namespace OpencartPages.Pages
         public IWebElement linkPound { get; set; }
 
         [FindsBy(How = How.CssSelector, Using = ".list-unstyled li h2")]
-        public IWebElement txtPrice { get; set; }   
+        public IWebElement txtPrice { get; set; }
+
+        //Review Form elements
+        [FindsBy(How = How.LinkText, Using = "Reviews (0)")]
+        public IWebElement linkTabReview { get; set; }
+        
+        [FindsBy(How = How.CssSelector, Using = ".form-group .col-sm-12 #input-name")]
+        public IWebElement nameInput { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".form-group .col-sm-12 #input-review")]
+        public IWebElement reviewInput { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[name=\"rating\"][value=\"5\"]")]
+        public IWebElement ratingInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "button-review")]
+        public IWebElement btnReview { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#form-review .alert-success")]
+        public IWebElement successAlert { get; set; } 
+
+        [FindsBy(How = How.CssSelector, Using = "#form-review .alert-danger")]
+        public IWebElement failAlert { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#form-review .alert-danger")]
+        public IWebElement nameAlert { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "#form-review .alert-danger")]
+        public IWebElement charactersAlert { get; set; } 
+
 
 
         public void SearchForItem(string text)
@@ -61,6 +93,34 @@ namespace OpencartPages.Pages
         public void PoundClick()
         {
             linkPound.Click();
+        }
+
+        //Review tab methods
+
+        public void TabReviewClick()
+        {
+            linkTabReview.Click();
+        }
+
+        public void WriteName(string text) 
+        {
+            nameInput.SendKeys(text);
+           
+        }
+
+        public void WriteReview(string text)
+        {
+            reviewInput.SendKeys(text); 
+        }
+
+        public void GiveRating() 
+        {
+            ratingInput.Click();
+        } 
+
+        public void clickReviewButton()
+        {
+            btnReview.Click();
         }
     }
 }
--- OpencartPages/TestingProductPage.cs
+++ OpencartPages/TestingProductPage.cs
@@ -62,7 +62,7 @@ namespace OpencartPages
             ProductPage productPage = new ProductPage(browser);
             productPage.SearchForItem("iPhone");
 
-            productPage.productResultClick();
+            productPage.productResultClick(); 
             productPage.CurrencyClick();
             productPage.PoundClick();
 
@@ -70,7 +70,93 @@ namespace OpencartPages
 
             var isMyPriceInPounds = productPage.txtPrice.Text.Contains("£");  //to find about characters 
 
-            Assert.IsTrue(isMyPriceInPounds);
+            Assert.IsTrue(isMyPriceInPounds); 
+
+        }
+
+        [TestMethod]
+        public void WriteValidReview()
+        {
+            //Very good product.I recommend
+            //to check " Thank you for your review. It has been submitted to the webmaster for approval."
+            ProductPage productPage = new ProductPage(browser);
+            productPage.SearchForItem("iPhone");
+            
+
+            productPage.productResultClick();
+            productPage.TabReviewClick();
+            productPage.WriteName("AslamAshishRmabadKumar"); 
+            productPage.WriteReview("Very good product. I recommend");
+            productPage.GiveRating();
+            productPage.clickReviewButton();
+
+            var reviewSuccessfully = productPage.successAlert.Text;
+
+            Assert.AreEqual(reviewSuccessfully, "Thank you for your review. It has been submitted to the webmaster for approval.");
+
+        }
+
+        [TestMethod]
+        public void WriteInvalidReviewRaiting()  
+        {
+            //Very good product.I recommend
+            //to check "Please select a review rating!"
+            ProductPage productPage = new ProductPage(browser);
+            productPage.SearchForItem("iPhone");
+
+
+            productPage.productResultClick();
+            productPage.TabReviewClick();
+            productPage.WriteName("AslamAshishRmabadKumar");
+            productPage.WriteReview("Very good product. I recommend");
+            productPage.clickReviewButton();
+
+            var reviewFail = productPage.failAlert.Text;
+
+            Assert.AreEqual(reviewFail, "Warning: Please select a review rating!");
+
+        }
+
+        [TestMethod]
+        public void WriteInvalidReviewName() 
+        {
+            //Very good product.I recommend
+            //to check "Please select a review rating!"
+            ProductPage productPage = new ProductPage(browser);
+            productPage.SearchForItem("iPhone");
+
+
+            productPage.productResultClick();
+            productPage.TabReviewClick();
+            productPage.WriteReview("Very good product. I recommend");
+            productPage.GiveRating();
+            productPage.clickReviewButton();
+
+            var reviewName = productPage.nameAlert.Text;  
+
+            Assert.AreEqual(reviewName, "Warning: Review Name must be between 3 and 25 characters!");
+
+        }
+
+        [TestMethod]
+        public void WriteInvalidReviewcharacters()
+        {
+            //Very good product.I recommend
+            //to check "Please select a review rating!"
+            ProductPage productPage = new ProductPage(browser);
+            productPage.SearchForItem("iPhone");
+
+
+            productPage.productResultClick();
+            productPage.TabReviewClick();
+            productPage.WriteName("AslamAshishRmabadKumar");
+            productPage.WriteReview("Very good.");
+            productPage.GiveRating();
+            productPage.clickReviewButton();
+
+            var reviewCharacters = productPage.charactersAlert.Text; 
+
+            Assert.AreEqual(reviewCharacters, "Warning: Review Text must be between 25 and 1000 characters!");
 
         }
     }
