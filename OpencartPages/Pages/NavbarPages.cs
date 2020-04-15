10b7ed5cc850bcb98b3d05baab7e5d9eaa27c77d
Ovidiu Chiruta <ovidiu.chiruta@gmail.com>
2020-04-16 00:26:25 +0300

    Added TestingNavbarPages - Containing 8 Test Cases

---
+++ OpencartPages/Pages/NavbarPages.cs
@@ -0,0 +1,172 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpencartPages.Pages
+{
+    public class NavbarPages
+    {
+        public NavbarPages(IWebDriver browser)  //LoginPage constructor
+        {
+            PageFactory.InitElements(browser, this);
+        }
+
+        //Navbar 
+        //Desktops Dropdown Elements
+        [FindsBy(How = How.LinkText, Using = "Desktops")]
+        public IWebElement linkDesktops { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "PC (0)")]
+        public IWebElement pcProductsLink { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"content\"]/p")]
+        public IWebElement txtNoProductsToList { get; set; }
+
+
+        //Laptops & Notebooks Dropdown Elements
+        [FindsBy(How = How.LinkText, Using = "Laptops & Notebooks")]
+        public IWebElement linkLaptopsAndNotebooks { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "Windows (0)")]
+        public IWebElement windowsLink { get; set; } 
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"content\"]/p")]
+        public IWebElement txtNoProductsToListLN { get; set; }
+
+
+        //Components Dropdown Elements
+        [FindsBy(How = How.LinkText, Using = "Components")]
+        public IWebElement linkComponents { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "Monitors (2)")]
+        public IWebElement linkMonitors { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "Samsung SyncMaster 941BW")]
+        public IWebElement txtMonitorsProdTitle { get; set; }
+
+
+        //Tablets Page Elements
+        [FindsBy(How = How.LinkText, Using = "Tablets")]
+        public IWebElement linkTablets { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "Samsung Galaxy Tab 10.1")]
+        public IWebElement txtTabletsProdTitle { get; set; }
+
+
+        //Software Page Elements
+        [FindsBy(How = How.LinkText, Using = "Software")]
+        public IWebElement linkSoftware { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"content\"]/p")]
+        public IWebElement txtNoProductsToListSof { get; set; }
+
+
+        //Phones and PDAs Page Elements
+        [FindsBy(How = How.LinkText, Using = "Phones & PDAs")]
+        public IWebElement linkPhonesPDAsPage { get; set; } 
+
+        [FindsBy(How = How.LinkText, Using = "Palm Treo Pro")]
+        public IWebElement txtPhonesPDAsProdTitle { get; set; }
+
+
+        //Cameras Page Elements
+        [FindsBy(How = How.LinkText, Using = "Cameras")]
+        public IWebElement linkCameras { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "Nikon D300")]
+        public IWebElement txtCamerasProdTitle { get; set; }
+
+
+        //MP3 Dropdown Elements
+        [FindsBy(How = How.LinkText, Using = "MP3 Players")]
+        public IWebElement linkMP3Players { get; set; }
+
+        [FindsBy(How = How.LinkText, Using = "test 4 (0)")]
+        public IWebElement linkTest4 { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id=\"content\"]/p")]
+        public IWebElement txtNoProductsToListMp3 { get; set; }
+
+
+
+        //Desktops Dropdown Methods
+        public void ClickOnDesktops()
+        {
+            linkDesktops.Click();
+        }
+
+        public void ClickOnPC()
+        {
+            pcProductsLink.Click();
+        }
+
+
+        //Laptops & Notebooks Dropdown Methods 
+        public void ClickOnLaptopsAndNotebooks()
+        {
+            linkLaptopsAndNotebooks.Click();
+        }
+
+        public void ClickOnWindows()
+        {
+            windowsLink.Click();
+        }
+
+
+        //Components Dropdown Methods 
+        public void ClickOnComponents()
+        {
+            linkComponents.Click(); 
+        }
+
+        public void ClickOnMonitors()
+        {
+            linkMonitors.Click();
+        }
+
+
+        //Tablets Page Methods 
+        public void ClickOnTablets()
+        {
+            linkTablets.Click();
+        }
+
+
+        //Software Page Methods 
+        public void ClickOnSoftwarePage()
+        {
+            linkSoftware.Click();
+        }
+
+
+        //PhonesPDAs Page Methods 
+        public void ClickOnPhonesPDAsPage()
+        {
+            linkPhonesPDAsPage.Click(); 
+        }
+
+
+        //Cameras Page Methods 
+        public void ClickOnCamerasPage()
+        {
+            linkCameras.Click();
+        }
+
+
+        //MP3 Dropdown Methods 
+        public void ClickOnMP3Players()
+        {
+            linkMP3Players.Click();
+        }
+
+        public void ClickOnTest4() 
+        {
+            linkTest4.Click();
+        }
+    }
+}
+
---
+++ OpencartPages/TestingNavbarPages.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpencartPages.Pages;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpencartPages
+{
+    [TestClass] 
+    public class TestingNavbarPages
+    {
+        private ChromeDriver browser;
+
+        [TestInitialize]  //MS Test tag initialize
+        public void TestInit()
+        {
+            //Open browser instance
+            browser = new ChromeDriver();
+
+            //Open page
+            browser.Navigate().GoToUrl("https://demo.opencart.com/");
+
+            //Implicit wait
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            //Close driver and browser
+            browser.Quit();
+        }
+
+        [TestMethod]
+        public void DesktopsDropdown() 
+        {
+
+            NavbarPages navbarPages = new NavbarPages(browser);
+            navbarPages.ClickOnDesktops();
+            navbarPages.ClickOnPC();
+
+            var checkPCPageMsg = navbarPages.txtNoProductsToList.Text;
+
+            Assert.AreEqual(checkPCPageMsg, "There are no products to list in this category.");
+        }
+
+        [TestMethod]
+        public void LaptopsAndNotebooksDropdown()
+        {
+
+            NavbarPages navbarPages = new NavbarPages(browser);
+            navbarPages.ClickOnLaptopsAndNotebooks();
+            navbarPages.ClickOnWindows();
+
+            var checkWindowsPageMsg = navbarPages.txtNoProductsToListLN.Text;
+
+            Assert.AreEqual(checkWindowsPageMsg, "There are no products to list in this category.");
+        }
+
+
+        [TestMethod]
+        public void ComponentsDropdown()
+        {
+
+            NavbarPages navbarPages = new NavbarPages(browser);
+            navbarPages.ClickOnComponents();
+            navbarPages.ClickOnMonitors();
+
+            var checkMonitorsProdTitle = navbarPages.txtMonitorsProdTitle.Text;
+
+            Assert.AreEqual(checkMonitorsProdTitle, "Samsung SyncMaster 941BW");
+        }
+
+
+        [TestMethod]
+        public void TabletsPage()
+        {
+
+            NavbarPages navbarPages = new NavbarPages(browser);
+            navbarPages.ClickOnTablets();
+
+            var checkTabletsProdTitle = navbarPages.txtTabletsProdTitle.Text;
+
+            Assert.AreEqual(checkTabletsProdTitle, "Samsung Galaxy Tab 10.1");
+        }
+
+
+        [TestMethod]
+        public void SoftwarePage()
+        {
+
+            NavbarPages navbarPages = new NavbarPages(browser);
+            navbarPages.ClickOnSoftwarePage();
+
+            var checkSoftwarePageMsg = navbarPages.txtNoProductsToListSof.Text; 
+
+            Assert.AreEqual(checkSoftwarePageMsg, "There are no products to list in this category.");
+        }
+
+
+        [TestMethod]
+        public void PhonesAndPDAsPage() 
+        {
+
+            NavbarPages navbarPages = new NavbarPages(browser);
+            navbarPages.ClickOnPhonesPDAsPage();
+
+            var checkPhonesPDAsProdTitle = navbarPages.txtPhonesPDAsProdTitle.Text; 
+
+            Assert.AreEqual(checkPhonesPDAsProdTitle, "Palm Treo Pro");
+        }
+
+
+        [TestMethod]
+        public void CamerasPage()
+        {
+
+            NavbarPages navbarPages = new NavbarPages(browser);
+            navbarPages.ClickOnCamerasPage(); 
+
+            var checkCamerasProdTitle = navbarPages.txtCamerasProdTitle.Text;
+
+            Assert.AreEqual(checkCamerasProdTitle, "Nikon D300");
+        }
+
+
+        [TestMethod]
+        public void MP3PlayersDropdown() 
+        {
+
+            NavbarPages navbarPages = new NavbarPages(browser);
+            navbarPages.ClickOnMP3Players(); 
+            navbarPages.ClickOnTest4();
+
+            var checkMp3PageMsg = navbarPages.txtNoProductsToListMp3.Text; 
+
+            Assert.AreEqual(checkMp3PageMsg, "There are no products to list in this category.");
+        }
+    }
+}
